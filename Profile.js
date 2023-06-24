427c64fa06d2f8de1cdf41a0893627dc46c957e0
suraj sharma <94784024+surajsharmacode@users.noreply.github.com>
2023-06-24 23:15:22 +0530

    profile

---
+++ Profile.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Profile = () => {
+  // Add your logic to fetch and display the user's profile information
+  return (
+    <div>
+     
+    </div>
+  );
+};
+
+export default Profile;
