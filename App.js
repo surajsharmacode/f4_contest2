e56d339532a685e5f30de505b17e01f56ab04e60
suraj sharma <94784024+surajsharmacode@users.noreply.github.com>
2023-06-24 23:13:17 +0530

    app

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Login from './Login';
+import Profile from './Profile';
+
+
+const App = () => {
+  return (
+   <div>
+    <Login/>
+    <Profile/>
+   </div>
+  );
+};
+
+export default App;
